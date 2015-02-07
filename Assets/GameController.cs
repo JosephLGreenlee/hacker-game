6faa90f78b787eec7aae0241eeec9efec4b5f292
Joseph Greenlee <josephlgreenlee@gmail.com>
2015-02-06 19:24:17 -0500

    Basic random node structure

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -3,11 +3,42 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	public Transform nodeTypesParent;
+
 	// Use this for initialization
 	void Start () {
-		Node[] nodeTypes = gameObject.GetComponentsInChildren<Node>();
+		//TODO: Let NodeTypes handle this behavior?
+		Node[] nodeTypes = nodeTypesParent.GetComponentsInChildren<Node>();
 		Node startNode = (Node)Instantiate(nodeTypes [Random.Range (0, nodeTypes.Length)]);
 		startNode.transform.position = PlayerStart.instance.transform.position;
+		startNode.transform.parent = this.transform;
+
+		Node firstNode = (Node)Instantiate(nodeTypes [Random.Range (0, nodeTypes.Length)]);
+		firstNode.transform.position = new Vector3(startNode.transform.position.x + .1f,
+		                                           startNode.transform.position.y + 2f,
+		                                           startNode.transform.position.z);
+		firstNode.transform.parent = startNode.transform;
+		LineRenderer firstLine = (LineRenderer)Instantiate (NodeLineRenderer.instance);
+		firstLine.SetPosition (0,startNode.transform.position);
+		firstLine.SetPosition (1, firstNode.transform.position);
+
+		Node secondNode = (Node)Instantiate(nodeTypes [Random.Range (0, nodeTypes.Length)]);
+		secondNode.transform.position = new Vector3(startNode.transform.position.x + 2f,
+		                                           startNode.transform.position.y + .1f,
+		                                            startNode.transform.position.z);
+		secondNode.transform.parent = startNode.transform;
+		LineRenderer secondLine = (LineRenderer)Instantiate (NodeLineRenderer.instance);
+		secondLine.SetPosition (0, startNode.transform.position);
+		secondLine.SetPosition (1, secondNode.transform.position);
+		
+		Node thirdNode = (Node)Instantiate(nodeTypes [Random.Range (0, nodeTypes.Length)]);
+		thirdNode.transform.position = new Vector3(startNode.transform.position.x + 1.5f,
+		                                           startNode.transform.position.y + 1.5f,
+		                                           startNode.transform.position.z);
+		thirdNode.transform.parent = startNode.transform;
+		LineRenderer thirdLine = (LineRenderer)Instantiate (NodeLineRenderer.instance);
+		thirdLine.SetPosition (0, startNode.transform.position);
+		thirdLine.SetPosition (1, thirdNode.transform.position);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/NodeLineRenderer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeLineRenderer : MonoBehaviour {
+
+	//Here is a private reference only this class can access
+	private static LineRenderer _instance;
+	
+	//This is the public reference that other classes will use
+	public static LineRenderer instance
+	{
+		get
+		{
+			//If _instance hasn't been set yet, we grab it from the scene!
+			//This will only happen the first time this reference is used.
+			if(_instance == null)
+				_instance = (LineRenderer)GameObject.FindObjectOfType<NodeLineRenderer>().renderer;
+			return _instance;
+		}
+	}
+}
